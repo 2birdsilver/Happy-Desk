7cea650f7b98195af678664baac61c6b692bdd6c
YOUNGEUN SUH <121986519+YOUNGEUN100@users.noreply.github.com>
2024-02-29 11:33:54 +0900

    fix: 메모 컨트롤러에 /api 추가
    url path와 api url 이 같아서 새로고침시에 json 파일이 노출됨. api 추가하여 문제해결

--- src/main/java/me/birdsilver/guestbook/domain/memo/controller/MemoController.java
+++ src/main/java/me/birdsilver/guestbook/domain/memo/controller/MemoController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/memo")
+@RequestMapping("/api/memo")
 public class MemoController {
 
     private final MemoService memoService;
