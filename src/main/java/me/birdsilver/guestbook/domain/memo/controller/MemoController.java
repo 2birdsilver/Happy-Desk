c11632d033600e164092a6bb6ab17f9a12339ab1
2birdsilver <seeun0426@gmail.com>
2024-03-19 10:55:08 +0900

    feat: 메모 작성할 때 시큐리티로 작성자 정보 넣기

--- src/main/java/me/birdsilver/guestbook/domain/memo/controller/MemoController.java
+++ src/main/java/me/birdsilver/guestbook/domain/memo/controller/MemoController.java
@@ -1,50 +1,60 @@
 package me.birdsilver.guestbook.domain.memo.controller;
 
+import jakarta.annotation.Nullable;
+import lombok.RequiredArgsConstructor;
 import me.birdsilver.guestbook.domain.memo.dto.AddMemoRequest;
 import me.birdsilver.guestbook.domain.memo.dto.DeleteMemoRequest;
 import me.birdsilver.guestbook.domain.memo.dto.UpdateMemoRequest;
 import me.birdsilver.guestbook.domain.memo.entity.Memo;
 import me.birdsilver.guestbook.domain.memo.service.MemoService;
-import org.apache.coyote.Response;
+import me.birdsilver.guestbook.domain.interns.entity.Intern;
+import me.birdsilver.guestbook.domain.interns.service.MemberService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/memo")
+@RequiredArgsConstructor
 public class MemoController {
 
     private final MemoService memoService;
+    private final MemberService memberService;
 
-    public MemoController(MemoService memoService) {
-        this.memoService = memoService;
-    }
 
+    // 메모 작성
     @PostMapping
-    public ResponseEntity<Memo> addMemo(@RequestBody AddMemoRequest request) {
+    public ResponseEntity<Memo> addMemo(@RequestBody AddMemoRequest request, @Nullable Principal principal) {
         if (request.getContent() == null || request.getContent().isEmpty()) {
             request.setContent(" ");
+        }
+
+        // 로그인한 작성자의 경우 => AuthenticatedWriter필드에 userId 저장
+        if (principal != null) {
+            String email = principal.getName();
+
+            Intern user = memberService.findByEmail(email);
+
+            request.setAuthenticatedWriter(user.getId());
+            request.setWriter(user.getName());
+
+
         }
         Memo savedMemo = memoService.save(request);
         return new ResponseEntity<>(savedMemo, HttpStatus.CREATED);
     }
 
-    @GetMapping
-    public List<Memo> getAllMemos() {
-        return memoService.findAll();
-    }
+
+//    @GetMapping
+//    public List<Memo> getAllMemos() {
+//        return memoService.findAll();
+//    }
 
 
-    // 메모 수정 페이지
-    @GetMapping("/update/{id}")
-    public ResponseEntity<Memo> getMemoById(@PathVariable long id) {
-        Memo memo = memoService.findById(id);
-        return ResponseEntity.ok(memo);
-    }
-
+    // 유저별 메모리스트 조회 페이지
     @GetMapping("/{recipientId}")
     public ResponseEntity<List<Memo>> getMemosByRecipient(@PathVariable Long recipientId) {
 
@@ -56,32 +66,62 @@ public class MemoController {
     }
 
 
-
-    @PostMapping("/delete")
-    public ResponseEntity<?> deleteMemo(@RequestBody DeleteMemoRequest request) {
-        Long memoId = request.getMemoId();
-        Memo memo = memoService.findById(memoId);
-
-        if (memo.getPassword().equals(request.getPassword())) {
-            memoService.delete(memoId);
-            return ResponseEntity.status(HttpStatus.ACCEPTED).build();
-        }
-        else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Password mismatch");
-        }
-
+    // 메모 수정 페이지
+    @GetMapping("/update/{id}")
+    public ResponseEntity<Memo> getMemoById(@PathVariable long id) {
+        Memo memo = memoService.findById(id);
+        return ResponseEntity.ok(memo);
     }
 
+
+    // 메모 수정
     @PutMapping("/{id}")
-    public ResponseEntity<?> updateMemo(@PathVariable long id, @RequestBody UpdateMemoRequest request) {
+    public ResponseEntity<?> updateMemo(@PathVariable long id, @RequestBody UpdateMemoRequest request, @Nullable Principal principal) {
         Memo memo = memoService.findById(id);
 
+        // 로그인한 작성자의 메모의 경우 => 본인 확인
+        if (principal != null) {
+            String email = principal.getName();
+            Long userId = memberService.findByEmail(email).getId();
+
+            if (request.getAuthenticatedWriter() != userId) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("not authorized");
+            }
+        }
+
+        // 비로그인한 작성자의 메모 => 비밀번호 확인
         if (!request.getPassword().equals(memo.getPassword())) {
-            // 비밀번호가 다를 경우 UNAUTHORIZED 상태와 에러 메시지를 반환
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Password mismatch");
         }
 
         Memo updatedMemo = memoService.update(id, request);
         return ResponseEntity.ok(updatedMemo);
     }
+
+
+    // 메모 삭제
+    @PostMapping("/delete")
+    public ResponseEntity<?> deleteMemo(@RequestBody DeleteMemoRequest request, @Nullable Principal principal) {
+        Long memoId = request.getMemoId();
+        Memo memo = memoService.findById(memoId);
+
+        // 로그인한 작성자의 메모의 경우 => 본인 확인
+        if (principal != null) {
+            String email = principal.getName();
+            Long userId = memberService.findByEmail(email).getId();
+
+            if (memo.getAuthenticatedWriter() != userId) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("not authorized");
+            }
+        }
+
+        // 비로그인한 작성자의 메모 => 비밀번호 확인
+        else if (!memo.getPassword().equals(request.getPassword())) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Password mismatch");
+        }
+
+        memoService.delete(memoId);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).build();
+    }
+
 }
